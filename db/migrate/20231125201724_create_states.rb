fb4b3289bd7d6f1aba4430e25984a71209f08a9f
Daniel <carreraprogrammer@gmail.com>
2023-11-25 15:17:30 -0500

    Add State model and migration

---
+++ db/migrate/20231125201724_create_states.rb
@@ -0,0 +1,10 @@
+class CreateStates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :states do |t|
+      t.string :name
+      t.references :country, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
