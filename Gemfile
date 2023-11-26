2762f4f44c4be78d67209d10af777a634e677218
Daniel <carreraprogrammer@gmail.com>
2023-11-25 21:23:28 -0500

    Add shoulda-matchers gem to Gemfile

--- Gemfile
+++ Gemfile
@@ -69,5 +69,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem 'rspec-rails'
-
+  gem 'shoulda-matchers'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :birth_date, presence: true
   validates :address, presence: true
